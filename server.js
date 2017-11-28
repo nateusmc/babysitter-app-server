ef43884dccacf7af9c6f0c7d7421311f85b594e1
Adam Bradbury <f.adambrad@gmail.com>
2017-11-28 10:18:56 -0700

    added methods() to models.js

---
+++ server.js
@@ -0,0 +1,72 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+ 
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {ParentalInfo} = require('./users/models');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
+
+app.use(
+  cors({
+    origin: DATABASE_URL
+  })
+);  
+
+app.get('/', (req, res) => {
+  console.log('server.js working')
+  res.send('stop running')
+})
+
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+    
+      return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+          if (err) {
+            return reject(err);
+          }
+          server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve();
+          })
+          .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+        });
+      });
+    }
+    
+
+    function closeServer() {
+        return mongoose.disconnect().then(() => {
+           return new Promise((resolve, reject) => {
+             console.log('Closing server');
+             server.close(err => {
+                 if (err) {
+                     return reject(err);
+                 }
+                 resolve();
+             });
+           });
+        });
+      }
+
+      if (require.main === module) {
+        runServer().catch(err => console.error(err));
+      };
+      
+      module.exports = {app, runServer, closeServer};
---
+++ users/models.js
@@ -0,0 +1,27 @@
+'use strict'
+const mongoose = require('mongoose');
+
+const ParentalInfoSchema = mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    ageOfChild: {type: Number, required: true},
+    zipcode: {type: String, required: true},
+    dateNeeded: {type: Date, required: true},
+    additionalInfo: {type: String}
+})
+
+ParentalInfoSchema.methods.apiRepr = function() {
+    return {
+        id: this._id,
+        firstName: this.firstName,
+        ageOfChild: this.ageOfChild,
+        zipcode: this.zipcode,
+        dateNeeded: this.dateNeeded,
+        additionalInfo: this.additionalInfo
+    }
+}
+
+
+const ParentalInfo = mongoose.model('ParentalInfo', ParentalInfoSchema);
+
+module.exports = {ParentalInfo};
