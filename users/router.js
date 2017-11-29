dc6a74af4b6ab3a90dfc894936da8647252babad
Adam Bradbury <f.adambrad@gmail.com>
2017-11-29 16:02:15 -0700

    readded .env file that was erased due to last pull

--- users/router.js
+++ users/router.js
@@ -10,11 +10,13 @@ const router = express.Router();
 const jsonParser = bodyParser.json();
 
 
-router.get('/', (req, res) => {
-    ParentalInfo
+router.post('/zipcode', jsonParser, (req, res) => {
+    console.log(req.body.zipcode)
+    return ParentalInfo
         .find()
+        .where('zipcode').equals(req.body.zipcode)
         .then(info => {
-            console.log('get request working')
+            console.log(info)
             res.json(info.map(data => data.apiRepr()))
         })
         .catch(err => {
@@ -23,42 +25,42 @@ router.get('/', (req, res) => {
         });
   });
 
-router.get('?zipcode', (req, res) => {
-    return ParentalInfo
-    .find()
-    .where('zipcode').equals(req.query.zipcode)
-    .then(result => {
-        res.json(result);
-    })
-})
+// router.get('?zipcode', (req, res) => {
+//     return ParentalInfo
+//     .find()
+//     .where('zipcode').equals(req.query.zipcode)
+//     .then(result => {
+//         res.json(result);
+//     })
+// })
   
 
-router.post('/', (req, res) => {
-const requiredFields = ['firstName', 'lastName', 'ageOfChild', 'zipcode', 'dateNeeded']
-for(let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if(!(field in req.body)) {
-    const message = `Missing \`${field}\` in request body`
-    console.error(message)
-    return res.status(400).send(message);
-    }
-}
+// router.post('/', (req, res) => {
+// const requiredFields = ['firstName', 'lastName', 'ageOfChild', 'zipcode', 'dateNeeded']
+// for(let i=0; i<requiredFields.length; i++) {
+//     const field = requiredFields[i];
+//     if(!(field in req.body)) {
+//     const message = `Missing \`${field}\` in request body`
+//     console.error(message)
+//     return res.status(400).send(message);
+//     }
+// }
 
-ParentalInfo
-    .create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        ageOfChild: req.body.ageOfChild,
-        zipcode: req.body.zipcode,
-        dateNeeded: req.body.dateNeeded,
-        additionalInfo: req.body.additionalInfo,
-    })
-    .then(parentInfo => res.status(201).json(parentInfo.apiRepr()))
-    .catch(err => {
-    console.error(err);
-    res.status(500).json({error: 'info not received'})
-    })
-});
+// ParentalInfo
+//     .create({
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         ageOfChild: req.body.ageOfChild,
+//         zipcode: req.body.zipcode,
+//         dateNeeded: req.body.dateNeeded,
+//         additionalInfo: req.body.additionalInfo,
+//     })
+//     .then(parentInfo => res.status(201).json(parentInfo.apiRepr()))
+//     .catch(err => {
+//     console.error(err);
+//     res.status(500).json({error: 'info not received'})
+//     })
+// });
 
 
   module.exports = { router };
