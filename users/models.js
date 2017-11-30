d53119ecdee0519bce494dbbea628060a2794eb9
Adam Bradbury <f.adambrad@gmail.com>
2017-11-30 15:49:56 -0700

    updated router.js to error if zipcode is too short or too long

--- users/models.js
+++ users/models.js
@@ -7,7 +7,10 @@ const ParentalInfoSchema = mongoose.Schema({
     ageOfChild: {type: Number, required: true},
     zipcode: {type: Number, required: true},
     dateNeeded: {type: Date, required: true},
-    additionalInfo: {type: String, required: false}
+    startTime: {type: String, require: true},
+    endTime: {type: String, require: true},
+    email: {type: String, require: true},
+    additionalInfo: {type: String, required: false},
 })
 
 ParentalInfoSchema.methods.apiRepr = function() {
@@ -19,6 +22,9 @@ ParentalInfoSchema.methods.apiRepr = function() {
         ageOfChild: this.ageOfChild,
         zipcode: this.zipcode,
         dateNeeded: this.dateNeeded,
+        startTime: this.startTime,
+        endTime: this.endTime,
+        email: this.email,
         additionalInfo: this.additionalInfo
     }
 }
--- users/router.js
+++ users/router.js
@@ -24,44 +24,90 @@ router.post('/zipcode', jsonParser, (req, res) => {
             res.status(500).json({error: 'server side error'});
         });
   });
-
-// router.get('?zipcode', (req, res) => {
-//     return ParentalInfo
-//     .find()
-//     .where('zipcode').equals(req.query.zipcode)
-//     .then(result => {
-//         res.json(result);
-//     })
-// })
   
 
 router.post('/', (req, res) => {
     console.log(req.body);
-const requiredFields = ['firstName', 'lastName', 'ageOfChild', 'zipcode', 'dateNeeded']
-for(let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if(!(field in req.body)) {
-    const message = `Missing \`${field}\` in request body`
-    console.error(message)
-    return res.status(400).send(message);
-    }
-}
+const requiredFields = ['firstName', 'lastName', 'ageOfChild', 'zipcode', 'dateNeeded', 'startTime','endTime', 'email']
+const missingField = requiredFields.find(field => !(field in req.body));
 
-ParentalInfo
-    .create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        ageOfChild: req.body.ageOfChild,
-        zipcode: req.body.zipcode,
-        dateNeeded: req.body.dateNeeded,
-        additionalInfo: req.body.additionalInfo,
-    })
-    .then(parentInfo => res.status(201).json(parentInfo.apiRepr()))
-    .catch(err => {
-    console.error(err);
-    res.status(500).json({error: 'info not received'})
-    })
-});
+    if(missingField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: 'Missing field',
+            location: missingField
+        })
+    }
+
+    // const numberFields = ['ageOfChild', 'zipcode',]
+    // const nonNumberFields = numberFields.find(
+    //     field => field in req.body && typeof req.body[field] !== 'number'
+    // );
+
+    // if(nonNumberFields) {
+    //     return res.status(422).json({
+    //         code: 422, 
+    //         reason: 'ValidationError',
+    //         message: 'Incorrect field type: expected number',
+    //         location: nonNumberFields
+    //     });
+    // }
+
+    const sizedFields = {
+        zipcode: {min: 5, max: 5}
+    }
+
+    const tooSmallField = Object.keys(sizedFields).find(field =>
+        'min' in sizedFields[field] && 
+        req.body[field].trim().length < sizedFields[field]. min
+    );
+
+    const tooLargeField = Object.keys(sizedFields).find(field =>
+        'max' in sizedFields[field] &&
+        req.body[field].trim().length > sizedFields[field].max
+    );
+
+    if(tooSmallField || tooLargeField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: tooSmallField
+                ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+                : `Must be at most  ${sizedFields[tooLargeField].max} characters long`,
+            location: tooSmallField || tooLargeField
+        })
+    }
+
+
+
+    for(let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if(!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message)
+        return res.status(400).send(message);
+        }
+    }
+
+    ParentalInfo
+        .create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            ageOfChild: req.body.ageOfChild,
+            zipcode: req.body.zipcode,
+            dateNeeded: req.body.dateNeeded,
+            startTime: req.body.startTime,
+            endTime: req.body.endTime,
+            email: req.body.email,
+            additionalInfo: req.body.additionalInfo,
+        })
+        .then(parentInfo => res.status(201).json(parentInfo.apiRepr()))
+        .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'info not received'})
+        })
+    });
 
 
   module.exports = { router };
